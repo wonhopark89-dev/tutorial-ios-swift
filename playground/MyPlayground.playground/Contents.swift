12f272d515ae734a0a20843e51fd8791832bad69
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-01-16 16:21:28 +0900

    스위프트 기본 문법(조건문)

--- playground/MyPlayground.playground/Contents.swift
+++ playground/MyPlayground.playground/Contents.swift
@@ -1,5 +1,49 @@
 import UIKit
 
+/*
+ 조건문
+ if, switch, guard
+ */
+
+let temperature = 30;
+
+// 범위
+switch temperature {
+    case -20...9:
+        print("겨울 입니다.")
+    case 10...14:
+        print("가을 입니다.")
+    case 15...25:
+        print("봄 입니다")
+    case 26...35:
+        print("여름 입니다")
+    default:
+        print("이상 기후 입니다.")
+}
+
+let color = "blue"
+// 값
+switch color {
+    case "blue":
+        print("파란색 입니다.")
+    case "green":
+        print("초록색 입니다")
+    case "yellow":
+        print("노란색 입니다")
+    default:
+        print("찾는 색상이 없습니다.") 
+}
+
+let animal = "cat"
+
+if animal == "cat" {
+    print("고양이 사료주기")
+} else if animal == "dog" {
+    print("강아지 사료주기")
+} else {
+    print("해당하는 동물이 없습니다.")
+}
+
 
 /*
  스위프트는 함수형 프로그래밍 패러다임을 포함하는 언어이다
