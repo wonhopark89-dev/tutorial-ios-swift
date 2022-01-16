ba0181d724c1f9bc3de9dc3f46d7e2238cc392b6
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-01-16 15:50:57 +0900

    스위프트 기본 문법(함수)

--- playground/MyPlayground.playground/Contents.swift
+++ playground/MyPlayground.playground/Contents.swift
@@ -1,6 +1,63 @@
 import UIKit
 
 
+/*
+ 스위프트는 함수형 프로그래밍 패러다임을 포함하는 언어이다
+ 스위프트의 함수는 일급 객체이다. 함수를 변수, 상수에 할당 할 수 있다.
+ 함수는 매개변수를 통해서도 전달할 수 있다.
+ */
+
+/*
+ 함수마다 가변 매게변수는 1개 밖에 가지지 못함
+ 0개 이상의 개수를 배열로 취급함
+ */
+func sendMessage2(me: String, friends: String...) -> String {
+    return "I`m \(me), my friend: \(friends)"
+}
+sendMessage2(me: "펭수", friends: "거북이", "사자","고양이")
+sendMessage2(me: "펭수")
+
+/*
+ func 함수 이름(전달인자 레이블: 매개변수 이름: 매개변수 타입, ... ) -> 반환 타입 {
+    return 반환 값
+ }
+ */
+func sendMessage(from myName:String, to name:String, etc added:String = "없음") -> String {
+    return "Hello \(name)!, I`M \(myName), wanna talk about \(added)"
+}
+
+sendMessage(from: "뽀로로", to: "루피")
+sendMessage(from: "뽀로로", to: "루피", etc: "크롱")
+
+// 기본값 설정
+func greeting(friend: String, me:String = "ABCD") {
+    print("Hello, \(friend)!, I`m \(me)")
+}
+
+greeting(friend: "철수")
+greeting(friend: "도라에몽", me: "진구")
+
+/*
+ func 함수명(파라미터 이름: 데이터 타입) -> 반환 타입 {
+    return 반환 값
+ }
+ */
+
+func printName() -> Void {
+    
+}
+
+// 리턴타입없을 때 생략가능
+func printName2() {}
+
+
+func sum(a:Int, b:Int) -> Int {
+    return a+b;
+}
+
+sum(a:5, b:10);
+
+
 // 컬렉션 ( Array, Dictionary, Set )
 // - 데이터들의 집합 묶음
 
