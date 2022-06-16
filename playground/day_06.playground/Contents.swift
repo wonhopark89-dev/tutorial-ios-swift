0e7278aaa08c3411f2588a032a992c2039dd73a8
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-06-16 23:58:01 +0900

    프로퍼티 옵저버

---
+++ playground/day_06.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var myAge = 0 {
+    willSet {
+        print("값이 설정될 예정이다. myAge: \(myAge)")
+    }
+    didSet {
+        print("값이 설정되었다. myAge: \(myAge)")
+    }
+}
+
+myAge = 10
